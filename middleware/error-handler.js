a23ffb0acbc4a567c61b514be2b9f1739b7aea6c
Michael Aigbovbiosa <67432950+mikeaig4real@users.noreply.github.com>
2021-12-29 19:55:13 +0100

    Update error-handler.js

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -8,20 +8,20 @@ const errorHandlerMiddleware = (err, req, res, next) => {
   }
 
   if (err.name === 'ValidationError') {
-    customError.msg = Object.values(err.errors)
+    customErr.msg = Object.values(err.errors)
       .map((item) => item.message)
       .join(',')
-    customError.statusCode = 400
+    customErr.statusCode = 400
   }
   if (err.code && err.code === 11000) {
-    customError.msg = `Duplicate value entered for ${Object.keys(
+    customErr.msg = `Duplicate value entered for ${Object.keys(
       err.keyValue
     )} field, please choose another value`
-    customError.statusCode = 400
+    customErr.statusCode = 400
   }
   if (err.name === 'CastError') {
-    customError.msg = `No item found with id : ${err.value}`
-    customError.statusCode = 404
+    customErr.msg = `No item found with id : ${err.value}`
+    customErr.statusCode = 404
   }
   return res.status(customErr.statusCode).json({ msg: customErr.msg });
 }
